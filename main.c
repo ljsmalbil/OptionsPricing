01210499402e0afa7d86460841e23ff804a066c9
ljsmalbil <louksmalbil@gmail.com>
2020-12-04 13:40:25 +0100

    put formula added

--- main.c
+++ main.c
@@ -6,10 +6,17 @@ int main() {
 
     printf("Hello, World!\n");
     float strike_price = 20;
-    float stock_price = 24;
+    float stock_price = 22;
 
-    float payoff = call_payoff(strike_price, stock_price);
-    printf("%.3f", payoff);
+    float c_payoff = call_payoff(strike_price, stock_price);
+    printf("%.3f", c_payoff);
+
+    printf("\n");
+
+    float p_payoff = put_payoff(strike_price, stock_price);
+    printf("%.3f", p_payoff);
+
+
     return 0;
 
 }
--- options.h
+++ options.h
@@ -9,10 +9,25 @@ float call_payoff (float strike_price, float stock_price) {
     /* call payoff is defined as: Ct = max(0, St - K), where
      * Ct denotes the payoff at time t, St the stock price
      * at maturity and K the strike price. */
+    float default_value = 0;
+
     if (strike_price < stock_price) {
         return stock_price - strike_price;
     } else {
-        return 0;
+        return default_value;
+    }
+}
+
+float put_payoff (float strike_price, float stock_price) {
+    /* put payoff is defined as: Ct = max(0, K - st), where
+     * Ct denotes the payoff at time t, St the stock price
+     * at maturity and K the strike price. */
+    float default_value = 0;
+
+    if (stock_price < strike_price) {
+        return strike_price - stock_price;
+    } else {
+        return default_value;
     }
 }
 
